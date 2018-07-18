dfc0b7d82d1fc3268485454f07c26975934facca
Jialu <lijialudew@gmail.com>
2018-07-18 15:53:13 -0500

    add gitignore

--- recipes/kftt/download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://www.phontron.com/kftt/download/kftt-data-1.0.tar.gz
-
-tar -zxvf kftt-data-1.0.tar.gz kftt-data-1.0/data/tok
-
--- test/__init__.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['XNMT_SETTINGS'] = 'unittest'
-import dynet_config
-
-dynet_config.set(random_seed=2)
